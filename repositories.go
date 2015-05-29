29291b2f405b4d016427cc7bd4266e1b5a6d1824
Duncan Mac-Vicar P <dmacvicar@suse.de>
2015-05-29 16:29:49 +0200

    set the credentials in the registration server url

--- repositories.go
+++ repositories.go
@@ -21,6 +21,7 @@ import (
 	"io/ioutil"
 	"encoding/json"
 	"net/http"
+	"net/url"
 )
 
 type Repository struct {
@@ -57,9 +58,9 @@ func ParseProduct(reader io.Reader) (Product, error) {
 // request product information to the registration server
 // url is the registration server url
 // installedProduct is the product you are requesting
-func RequestProduct(url string, credentials Credentials, installed InstalledProduct) (Product, error) {
+func RequestProduct(regUrl string, credentials Credentials, installed InstalledProduct) (Product, error) {
 	client := &http.Client{}
-	req, err := http.NewRequest("GET", url, nil)
+	req, err := http.NewRequest("GET", regUrl, nil)
 
 	values := req.URL.Query()
 
@@ -68,6 +69,9 @@ func RequestProduct(url string, credentials Credentials, installed InstalledProd
 	values.Add("arch", installed.Arch)
 	req.URL.RawQuery = values.Encode()
 	req.URL.Path = "/connect/systems/products"
+
+	auth := url.UserPassword(credentials.Username, credentials.Password)
+	req.URL.User = auth
 
 	resp, err := client.Do(req)
 	if err != nil {
