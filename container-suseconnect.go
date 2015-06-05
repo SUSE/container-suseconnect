629a134ead9e21b08244e94a3e06079f135dd143
Miquel Sabaté Solà <msabate@suse.de>
2015-06-05 14:13:52 +0200

    Fix the license comments, so godoc.org doesn't get confused by them

--- container-suseconnect.go
+++ container-suseconnect.go
@@ -1,4 +1,3 @@
-//
 // Copyright (c) 2015 SUSE LLC. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,7 +11,9 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-//
+
+// Gives access to repositories during docker build and run using the host
+// machine credentials.
 package main
 
 import (
