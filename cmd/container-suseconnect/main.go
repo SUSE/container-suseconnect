b9a0a301c43c17230a7c7e59a78b768435764957
Ralf Haferkamp <rhafer@suse.com>
2019-11-27 16:44:56 +0100

    Update urfave/cli to v2.0.0

--- cmd/container-suseconnect/main.go
+++ cmd/container-suseconnect/main.go
@@ -24,7 +24,7 @@ import (
 	"time"
 
 	cs "github.com/SUSE/container-suseconnect/internal"
-	"github.com/urfave/cli"
+	"github.com/urfave/cli/v2"
 )
 
 func main() {
@@ -61,7 +61,7 @@ func main() {
 	}
 
 	// Set additional actions, which are always available
-	app.Commands = []cli.Command{
+	app.Commands = cli.Commands{
 		{
 			Name:    "list-products",
 			Aliases: []string{"lp"},
