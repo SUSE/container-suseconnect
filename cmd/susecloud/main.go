42407e9e5183cb3d42fd0e98801e3b848044608c
Miquel Sabaté Solà <msabate@suse.com>
2020-05-18 15:52:30 +0200

    cmd/susecloud: added the zypper plugin
    Signed-off-by: Miquel Sabaté Solà <msabate@suse.com>

---
+++ cmd/susecloud/main.go
@@ -0,0 +1,117 @@
+// Copyright (c) 2020 SUSE LLC. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Zypper url resolver plugin for /susecloud schemes.
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net/url"
+	"os"
+	"strings"
+	"time"
+
+	cs "github.com/SUSE/container-suseconnect/internal"
+	"github.com/SUSE/container-suseconnect/internal/susebuild"
+	"github.com/urfave/cli/v2"
+)
+
+const regionServerConfig = "/etc/regionserverclnt.cfg"
+
+func main() {
+	app := cli.NewApp()
+	app.Copyright = fmt.Sprintf("(c) %d SUSE LCC", time.Now().Year())
+	app.Name = "susecloud"
+	app.Version = cs.Version
+	app.Usage = ""
+	app.UsageText =
+		`Zypper URL resolver plugin that transforms plugin:/susecloud urls into
+	their proper counterparts inside of a SUSE container.`
+	app.Action = runZypperUrlResolver
+
+	log.SetOutput(cs.GetLoggerFile())
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
+
+// The main action: reads the arguments as given by zypper on the stdin, and
+// prints into stdout the response to be used.
+func runZypperUrlResolver(_ *cli.Context) error {
+	if err := susebuild.ServerReachable(); err != nil {
+		return fmt.Errorf("Could not reach build server from the host: %v", err)
+	}
+
+	input, err := parseStdin()
+	if err != nil {
+		return fmt.Errorf("Could not parse input: %s", err)
+	}
+
+	return printResponse(input)
+}
+
+// Parses the standard input as given by zypper and returns a map with the
+// parsed parameters.
+func parseStdin() (map[string]string, error) {
+	params := make(map[string]string)
+	first := true
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		if first {
+			first = false
+		} else {
+			vals := strings.SplitN(scanner.Text(), ":", 2)
+			if len(vals) == 2 {
+				params[vals[0]] = vals[1]
+			}
+		}
+	}
+
+	if scanner.Err() != nil {
+		return nil, scanner.Err()
+	}
+
+	return params, nil
+}
+
+// Prints to standard output with the format expected by zypper.
+func printResponse(params map[string]string) error {
+	cfg, err := susebuild.ReadConfigFromServer()
+	if err != nil {
+		return err
+	}
+
+	u, err := url.Parse(cfg.Server)
+	if err != nil {
+		return err
+	}
+
+	q := u.Query()
+	for k, v := range params {
+		q.Add(k, v)
+	}
+	u.RawQuery = q.Encode()
+
+	log.Printf("Resulting X-Instance-Data: %s", cfg.InstanceData)
+	log.Printf("Resulting URL: %s", u.String())
+
+	fmt.Println("RESOLVEDURL:")
+	fmt.Printf("X-Instance-Data:%s\n", cfg.InstanceData)
+	fmt.Println(u.String())
+
+	return nil
+}
---
+++ internal/susebuild/server.go
@@ -0,0 +1,100 @@
+// Copyright (c) 2020 SUSE LLC. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Utilities for talking to a suse-build server.
+package susebuild
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net"
+	"os"
+)
+
+// SuseBuildConfig contains all the data that is available through the
+// suse-build server running on the host.
+type SuseBuildConfig struct {
+	InstanceData string `json:"instance-data"`
+	Server       string `json:"server"`
+}
+
+// suseBuildAddress returns a string containing the full address of the TCP
+// server. You may tweak this by providing `SUSE_BUILD_IP` and/or
+// `SUSE_BUILD_PORT`, otherwise `0.0.0.0` and `7956` are taken as defaults for
+// the IP and the port respectively.
+func suseBuildAddress() string {
+	ip := os.Getenv("SUSE_BUILD_IP")
+	if ip == "" {
+		ip = "0.0.0.0"
+	}
+
+	port := os.Getenv("SUSE_BUILD_PORT")
+	if port == "" {
+		port = "7956"
+	}
+
+	return fmt.Sprintf("%s:%s", ip, port)
+}
+
+// ServerAvailable returns true if the suse-build server is reachable, false
+// otherwise.
+func ServerReachable() error {
+	addr, err := net.ResolveTCPAddr("tcp", suseBuildAddress())
+	if err != nil {
+		return err
+	}
+
+	conn, err := net.DialTCP("tcp", nil, addr)
+	if err != nil {
+		return err
+	}
+
+	_ = conn.Close()
+	return nil
+}
+
+// ReadConfigFromServer performs a request agains the suse-build server running
+// in the host, and it parses the given response so it can be used as a
+// SuseBuildConfig.
+func ReadConfigFromServer() (*SuseBuildConfig, error) {
+	addr, err := net.ResolveTCPAddr("tcp", suseBuildAddress())
+	log.Printf("Trying to reach suse build server at '%v'", addr.String())
+	if err != nil {
+		return nil, err
+	}
+
+	conn, err := net.DialTCP("tcp", nil, addr)
+	if err != nil {
+		return nil, err
+	}
+
+	defer conn.Close()
+	log.Printf("Reading from suse build server...")
+
+	// After testing it turns out that we need something a bit over 2048, but
+	// let's leave some extra room just in case...
+	reply := make([]byte, 4096)
+
+	n, err := conn.Read(reply)
+	if err != nil {
+		return nil, err
+	}
+
+	data := &SuseBuildConfig{}
+	if err := json.Unmarshal(reply[:n], &data); err != nil {
+		return nil, err
+	}
+	return data, nil
+}
